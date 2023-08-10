9046d1ce2e17946dbb4d44845c1e91491462b3b4
Arun Mathai <me@arunmathaisk.in>
2023-08-10 21:44:46 +0530

    fix: UI fix

--- frontend/src/components/SideNavbar.vue
+++ frontend/src/components/SideNavbar.vue
@@ -1,33 +1,31 @@
 <template>
-  <div
-    class="flex flex-col justify-between bg-white h-screen md:w-1/6 shadow-lg rounded-lg overflow-y-auto"
-  >
-    <div class="p-6 w-full bg-gray-100">
-      <h1 class="text-blue-500 text-4xl md:text-5xl font-semibold mb-6">
-        FlashDesk
-      </h1>
-      <ul class="space-y-3">
-        <li v-for="item in menuItems" :key="item.id">
-          <router-link
-            :to="item.path"
-            class="text-gray-800 text-xl md:text-2xl hover:text-blue-500"
-            >{{ item.title }}</router-link
-          >
-        </li>
-      </ul>
-    </div>
-    <footer class="p-6 w-full bg-gray-100 text-center">
-      <p class="text-gray-600 text-lg">
-        Welcome, <span class="font-semibold">{{ currentUser }}</span
-        > 😊
-      </p>
-      <router-link
-        to="/logout"
-        class="text-blue-500 mt-4 inline-block px-4 py-2 bg-blue-100 rounded-full hover:bg-blue-200 transition-colors"
-      >
-        Logout
-      </router-link>
-    </footer>
+  <div class="flex h-screen bg-gray-100">
+    <nav class="w-1/6 bg-white shadow-lg rounded-r-lg overflow-y-auto">
+      <div class="p-6">
+        <h1 class="text-3xl font-semibold text-blue-500 mb-6">FlashDesk</h1>
+        <ul class="space-y-3">
+          <li v-for="item in menuItems" :key="item.id">
+            <router-link
+              :to="item.path"
+              class="block text-gray-800 hover:text-blue-500"
+            >
+              {{ item.title }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <footer class="p-6 text-center">
+        <p class="text-gray-600 text-lg">
+          Welcome, <span class="font-semibold">{{ currentUser }}</span> 😊
+        </p>
+        <router-link
+          to="/logout"
+          class="block mt-4 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors"
+        >
+          Logout
+        </router-link>
+      </footer>
+    </nav>
   </div>
 </template>
 
@@ -39,8 +37,8 @@ export default {
   computed: {
     currentUser() {
       const userStore = useUserStore()
-      const user = userStore.getUser // Assuming getUser is a getter in your store module
-      return user.full_name // Update the property as per your store structure
+      const user = userStore.getUser
+      return user.full_name 
     },
   },
   data() {
