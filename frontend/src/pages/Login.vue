702cfae58e2d99ccd25c821dfc0bd907a53b247a
Arun Mathai <me@arunmathaisk.in>
2023-08-10 20:53:14 +0530

    feat: user store created

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,13 +1,36 @@
 <template>
-  <LoginBox title="Log in to FlashDesk" :class="{ 'pointer-events-none': loading }">
+  <LoginBox
+    title="Log in to FlashDesk"
+    :class="{ 'pointer-events-none': loading }"
+  >
     <form class="flex flex-col" @submit.prevent="">
-      <Input v-model="email" class="mb-4" label="Email" placeholder="johndoe@mail.com" name="email" autocomplete="email"
-        :type="email !== 'Administrator' ? 'email' : 'text'" required />
+      <Input
+        v-model="email"
+        class="mb-4"
+        label="Email"
+        placeholder="johndoe@mail.com"
+        name="email"
+        autocomplete="email"
+        :type="email !== 'Administrator' ? 'email' : 'text'"
+        required
+      />
 
-      <Input v-model="password" label="Password" type="password" placeholder="••••••••" name="password"
-        autocomplete="current-password" required />
+      <Input
+        v-model="password"
+        label="Password"
+        type="password"
+        placeholder="••••••••"
+        name="password"
+        autocomplete="current-password"
+        required
+      />
       <ErrorMessage :message="errorMessage" class="mt-4" />
-      <Button class="mt-4 focus:ring-0 focus:ring-offset-0" :loading="loading" appearance="primary" @click="login">
+      <Button
+        class="mt-4 focus:ring-0 focus:ring-offset-0"
+        :loading="loading"
+        appearance="primary"
+        @click="login"
+      >
         Submit
       </Button>
     </form>
@@ -18,6 +41,7 @@
 import { Input, ErrorMessage } from 'frappe-ui'
 import LoginBox from '@/components/LoginBox.vue'
 import { useAuthStore } from '@/stores/auth.js'
+import { useUserStore } from '@/stores/user.js'
 
 export default {
   name: 'Login',
@@ -38,7 +62,7 @@ export default {
       const data = {
         usr: this.email,
         pwd: this.password,
-      };
+      }
 
       fetch('/api/method/login', {
         method: 'POST',
@@ -50,11 +74,24 @@ export default {
         .then((response) => response.json())
         .then((responseData) => {
           if (responseData.message === 'Logged In') {
-            const authStore = useAuthStore();
-            authStore.login();
-            this.$router.push('/');
+            const authStore = useAuthStore()
+            const userStore = useUserStore()
+            let getCookies = () => {
+              return Object.fromEntries(
+                document.cookie
+                  .split('; ')
+                  .map((cookie) => cookie.split('='))
+                  .map((entry) => [entry[0], decodeURIComponent(entry[1])])
+              )
+            }
+            userStore.$patch((state) => {
+              state.user.user_id = getCookies().user_id,
+              state.user.full_name = getCookies().full_name
+            })
+            authStore.login()
+            this.$router.push('/')
           } else {
-            this.errorMessage = 'Invalid credentials';
+            this.errorMessage = 'Invalid credentials'
           }
         })
     },
---
+++ frontend/src/stores/user.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia';
+import { useStorage } from '@vueuse/core';
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user : useStorage('user',{
+        user_id : null,
+        full_name : null,
+    })
+  }),
+  getters: {
+    getUser: (state) => state.user,
+  },
+});
