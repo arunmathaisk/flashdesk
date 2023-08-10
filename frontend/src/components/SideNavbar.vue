cb15e43244d0528bd4959c2bfb6a1d11644c7ea0
Arun Mathai <me@arunmathaisk.in>
2023-08-10 22:17:26 +0530

    feat: better UI , feather icons now available

--- frontend/src/components/SideNavbar.vue
+++ frontend/src/components/SideNavbar.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="flex h-screen bg-gray-100">
-    <nav class="w-1/6 bg-white shadow-lg rounded-r-lg overflow-y-auto">
+    <nav class="w-1/5 bg-white shadow-lg rounded-r-lg overflow-y-auto">
       <div class="p-6">
-        <h1 class="text-3xl font-semibold text-blue-500 mb-6">FlashDesk</h1>
-        <ul class="space-y-3">
+        <h1 class="text-4xl font-semibold text-blue-600 mb-8">FlashDesk</h1>
+        <ul class="space-y-4">
           <li v-for="item in menuItems" :key="item.id">
             <router-link
               :to="item.path"
-              class="block text-gray-800 hover:text-blue-500"
+              class="block py-3 px-4 text-gray-800 hover:text-blue-600 transition-colors border-l-4 border-transparent hover:border-blue-500 {{ activeMenuItem(item.path) }}"
             >
               {{ item.title }}
             </router-link>
+            <div class="h-px bg-sky-500"></div>
+            <!-- Line of distinction -->
           </li>
         </ul>
       </div>
       <footer class="p-6 text-center">
         <p class="text-gray-600 text-lg">
-          Welcome, <span class="font-semibold">{{ currentUser }}</span> 😊
+          Welcome, <span class="font-semibold">{{ currentUser }} 😊</span>
         </p>
         <router-link
           to="/logout"
-          class="block mt-4 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors"
+          class="block mt-6 px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors"
         >
           Logout
         </router-link>
@@ -31,6 +33,7 @@
 
 <script>
 import { useUserStore } from '@/stores/user.js'
+import { FeatherIcon } from 'vue-feather-icons'
 
 export default {
   name: 'SideNavbar',
@@ -38,16 +41,15 @@ export default {
     currentUser() {
       const userStore = useUserStore()
       const user = userStore.getUser
-      return user.full_name 
+      return user.full_name
     },
   },
   data() {
     return {
       menuItems: [
-        { id: 1, title: '', path: '/' },
-        { id: 2, title: 'About', path: '/about' },
-        { id: 3, title: 'Contact', path: '/contact' },
-        { id: 3, title: 'Say hello', path: '/hello' },
+        { id: 1, title: 'Create Pod', path: '/about' },
+        { id: 2, title: 'Available Pods ', path: '/contact' },
+        { id: 2, title: 'Running Pods ', path: '/contact' },
       ],
     }
   },
