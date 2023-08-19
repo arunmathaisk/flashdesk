57386b43b24c63334b87a16843caa285cf2129ab
Arun Mathai <me@arunmathaisk.in>
2023-08-19 14:08:06 +0530

    chore : delete to kill

--- flashdesk/docker_utils/docker_client.py
+++ flashdesk/docker_utils/docker_client.py
@@ -23,7 +23,7 @@ def start_container_using_image_id(image_id):
     return container.short_id
 
 
-def delete_container_using_container_id(container_id):
+def kill_container_using_container_id(container_id):
     client.containers.get(container_id).kill()
 
 def get_all_docker_images():
