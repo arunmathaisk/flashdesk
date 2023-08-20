814c09cfb9b27c7da05d6eac065431c984346bbe
Arun Mathai <me@arunmathaisk.in>
2023-08-20 17:27:24 +0530

    fix : ducktape fix

--- frontend/src/pages/ActivePodImages.vue
+++ frontend/src/pages/ActivePodImages.vue
@@ -108,7 +108,7 @@ export default {
             text: 'Pod Run Sucessfully',
             showCancelButton: 'true'
           });
-          
+          const newTab = window.open("http://localhost"+":6901", "_blank");
           this.fetchActiveImages();
         } else {
           this.$swal({
