54c8ca49a7787d6247ed79bb56f29c7de941ea44
Arun Mathai <me@arunmathaisk.in>
2023-10-07 17:44:03 +0530

    chore : new vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,61 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
-import { webserver_port } from '../../../sites/common_site_config.json'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { webserver_port } from "../../../sites/common_site_config.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
     port: 8080,
-    proxy: getProxyOptions({ port: webserver_port }),
+    proxy: {
+      "^/(app|login|api|assets|files)": {
+        // Localhost resolution changes in Node 17
+        target: `http://127.0.0.1:${webserver_port}`,
+        ws: true,
+        router: function (req) {
+          const site_name = req.headers.host.split(":")[0];
+          return `http://${site_name}:${webserver_port}`;
+        },
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
+      "tailwind.config.js": path.resolve(__dirname, "tailwind.config.js"),
     },
   },
   build: {
-    outDir: `../${path.basename(path.resolve('..'))}/public/frontend`,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    outDir: `../${path.basename(path.resolve(".."))}/public/frontend`,
     emptyOutDir: true,
-    target: 'es2015',
+    target: "es2015",
+    commonjsOptions: {
+      include: [/tailwind.config.js/, /node_modules/],
+    },
   },
   optimizeDeps: {
-    include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
+    include: [
+      "frappe-ui",
+      "feather-icons",
+      "showdown",
+      "engine.io-client",
+      "tailwind.config.js",
+    ],
   },
-})
+});
