368d02dc8cde2a089cf34a65014f695af5fbdfdb
Arun Mathai <me@arunmathaisk.in>
2023-10-09 19:23:13 +0530

    feat : websockets now work

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,13 +7,21 @@ import { createPinia } from 'pinia';
 import { Button, setConfig, frappeRequest, resourcesPlugin } from 'frappe-ui'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-
+import { initSocket } from './socket'
 
 
 let app = createApp(App)
 const pinia = createPinia();
 
+
 setConfig('resourceFetcher', frappeRequest)
+
+
+let socket
+
+socket = initSocket()
+
+app.config.globalProperties.$socket = socket
 
 app.use(router)
 app.use(pinia);
@@ -21,4 +29,3 @@ app.use(resourcesPlugin)
 app.use(VueSweetalert2)
 app.component('Button', Button)
 app.mount('#app')
-app.config.globalProperties.$test = "hahaha"
