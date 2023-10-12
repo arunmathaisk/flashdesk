8c9ae899d66c1029269f6822fd9cf09aad4a44e8
Arun Mathai <me@arunmathaisk.in>
2023-10-12 13:20:45 +0530

    fix : broken app install script

--- install_script.sh
+++ install_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-bench init --frappe-branch develop bench 
+bench init --frappe-branch develop bench
 
 cd bench
 
-bench get-app https://github.com/arunmathaisk/flashdesk
-
 bench new-site flashdesk.site --admin-password apple --db-root-password apple
-
-bench --site flashdesk.site add-to-hosts
-
-bench --site flashdesk.site install-app flashdesk
 
 bench use flashdesk.site
 
 bench set-config -g developer_mode 1
+
+bench --site flashdesk.site add-to-hosts
+
+bench get-app https://github.com/arunmathaisk/flashdesk
+
+bench --site flashdesk.site install-app flashdesk
