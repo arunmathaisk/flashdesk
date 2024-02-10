911fe1a33a6a86abda413c613e553b43cfc095b1
Arun Mathai <me@arunmathaisk.in>
2024-02-10 18:50:58 +0530

    backend: pod commit

--- flashdesk/docker_utils/docker_low_level_client.py
+++ flashdesk/docker_utils/docker_low_level_client.py
@@ -9,11 +9,16 @@ def tar_image_create(filename, image_name):
     try:
         tar_file = os.path.join(filename)
         image_name = image_name.replace(' ', '').lower()
-
         if not os.path.isfile(tar_file):
             print("File does not exist:", tar_file)
-        
         result = ll_client.import_image_from_file(tar_file, repository=image_name)
-    
     except Exception as e:
         print("Exception occurred:", str(e))
+
+
+def save_container(container_id,container_params):
+    try:
+        com_container = ll_client.commit(container=container_id,tag=container_params.tag,message=container_params.message,pause=container_params.pause)
+    except Exception as e:
+        print("Exception occurred:", str(e))
+
