53b0d242544a0bd3a78a41dfa06a75ae0ad11fed
sokumon <sohamkulkarns9@gmail.com>
2024-02-15 14:40:06 +0530

    :fix hardcoding

--- flashdesk/api/ai/process_pdfs.py
+++ flashdesk/api/ai/process_pdfs.py
@@ -17,21 +17,27 @@ from langchain.callbacks.manager import CallbackManagerForLLMRun
 
 def create_vector_db():
     pdf_dir = frappe.get_site_path("private/files/pdffiles/")
+
     loader = PyPDFDirectoryLoader(pdf_dir)
     documents = loader.load()
-    base_url = "https://e6f9-103-123-226-98.ngrok-free.app"
+    base_url = "http://192.168.192.194:11434"
     print(f"Processed {len(documents)} pdf files")
+    chromadb_path = frappe.get_site_path("private/files/chromadb/")
+    os.makedirs(os.path.dirname(chromadb_path), exist_ok=True)
+
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
+
     texts=text_splitter.split_documents(documents)
-    vectorstore = Chroma.from_documents(documents=texts, embedding=OllamaEmbeddings(base_url=base_url, model="llama2"),persist_directory="/home/soham/vectortest/")      
+    vectorstore = Chroma.from_documents(documents=texts, embedding=OllamaEmbeddings(base_url=base_url, model="llama2"),persist_directory=os.path.abspath(chromadb_path))      
     vectorstore.persist()
 
 def fetch_info(query):
     print("sadasd")
-    base_url = "https://e6f9-103-123-226-98.ngrok-free.app"
+    base_url = "http://192.168.192.194:11434"
     ollama = Ollama(base_url=base_url,model="llama2")
     oembed = OllamaEmbeddings(base_url=base_url, model="llama2")
-    vectordb = Chroma(persist_directory="/home/soham/chroma", embedding_function=oembed)
+    chromadb_path = frappe.get_site_path("private/files/chromadb/")
+    vectordb = Chroma(persist_directory=os.path.abspath(chromadb_path), embedding_function=oembed)
     qachain=RetrievalQA.from_chain_type(ollama, retriever=vectordb.as_retriever())
     docs = vectordb.similarity_search(query, k = 2)
     print(len(docs))
